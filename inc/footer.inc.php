33f6c7548dca7253da98fc1ed6aca2a35ddd9825
Envel Le Hir <envel@lehir.net>
2016-06-25 12:25:16 +0200

    sparql queries: link to WDQS

--- inc/footer.inc.php
+++ inc/footer.inc.php
@@ -5,7 +5,7 @@ if (count($queries) >= 1) {
     echo '<h2>Requête'.((count($queries) >= 2) ? 's' : '').' SPARQL</h2>';
     $i = 0;
     foreach ($queries as $query) {
-        echo '<h3>#'.(++$i).'</h3><pre>'.htmlentities(trim($query)).'</pre>';
+        echo '<h3>#'.(++$i).' [<a href="https://query.wikidata.org/#'.rawurlencode($query).'">WDQS</a>]</h3><pre>'.htmlentities(trim($query)).'</pre>';
     }
 }
 
